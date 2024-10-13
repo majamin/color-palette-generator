0c15d224d025e41d71d16231551f07486748a34e
Marian Minar <majamin@gmail.com>
2024-10-13 10:24:29 -0700

    Big update
    - handle file input, add messages.
    - adding rows of swatches and text for easier comparison
    - added jetbrainsmono for convenience.
    - fixed _imagemagick_ dependency for the actual binary (magick)
    - replaced _convert_ with _magick_
    - removed invalid colors from colors.txt
    - consistent variable case in colors.sh

--- README.md
+++ README.md
@@ -4,34 +4,49 @@ A shell script that generates color palettes in PNG format.
 Other than the dependencies, this is a turn-key script.
 Instructions on how to download and process the JSON data use is described in `colors.sh`.
 
-The original repo that inspired this script: https://github.com/Jam3/nice-color-palettes
+[This](https://github.com/Jam3/nice-color-palettes) is original repo that inspired this script.
 
-# Instructions
+## Instructions
 
-You'll need `cat` (present on most systems), `ImageMagick`, and `bc`. Optionally, `curl` and `jq`.
+You'll need `cat` (present on most systems), `ImageMagick`, and `bc`.
+Optionally, `curl` and `jq` to retrieve the original color schemes.
 
 1. `git clone https://github.com/majamin/color-palette-generator`
 1. `cd color-palette-generator`
 1. `chmod +x colors.sh`
-1. `./colors.sh`
+1. `./colors.sh colors.txt`
 
-# What it does
+## What it does
 
 1. Uses ImageMagick to convert each color to a rectangle and combines.
 1. Uses ImageMagick to create a nice "rounded rectangle" image mask.
-1. Calculates whether a black or white font color should be used depending on color.
+1. Adds black or white HEX text to each color on bottom row.
+1. Adds each permutation to HEX text to each color on middle row.
+1. Adds color swatch of each color to top row.
 1. Writes output to a separate directory.
 
-# Preview
+## Preview
 
 One:
 
-![One example](example.png)
+![One example](one.png)
 
 Many:
 
-![Gallery](short-gallery.png)
+![Gallery](many.png)
 
-# Issues
+## Issues
 
 PRs are welcome.
+
+## Updates
+
+(2024-10-13)
+
+- Adding rows of swatches and text for easier comparison
+- Handle file input, add messages.
+- Added JetBrainsMono for convenience.
+- Fixed _ImageMagick_ dependency for the actual binary (magick)
+- Replaced _convert_ with _magick_
+- Removed invalid colors from colors.txt
+- Consistent variable case in colors.sh
--- colors.sh
+++ colors.sh
@@ -1,88 +1,183 @@
 #!/bin/bash
 # Creates color palettes based on TEXT or JSON data
-# Author: majamin <majamin at gmail dot com>
+# Author: Marian Minar <majamin at gmail dot com>
 # License: MIT
+
+# Usage: ./colors.sh <COLFILE>
+# See colors.txt for an example of <COLFILE>.
 
 # You can run this script as-is, but if you want to grab the original data
 # here are the instructions:
 
 # First grab some JSON-formatted data and write 'colors.json'
-#	curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" \
-#	"http://www.colourlovers.com/api/palettes/top?format=json&numResults=100&resultOffset=0" |\
-#	jq . > colors.json
+# curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" \
+#   "http://www.colourlovers.com/api/palettes/top?format=json&numResults=100&resultOffset=0" |
+#   jq . >colors.json
 
 # With the generate 'colors.json' file, turn into plain text:
 #
-#	cat colors{0,1,2,3,4,5,6,7,8,9}.json |\
-#	jq --raw-output '.[].colors |\
-#	@tsv' > colors.txt
+# cat colors{0,1,2,3,4,5,6,7,8,9}.json |
+#   jq --raw-output '.[].colors |\ @tsv' >colors.txt
 
 #-----------------------------------------------------------------------------
-# Dependencies: cat, ImageMagick, bc
+# Dependencies: cat, magick, bc
 cmd_failures=''
-for cmd in cat ImageMagick bc; do
+for cmd in cat magick bc; do
   command -v "${cmd}" >/dev/null 2>&1 || cmd_failures="${cmd_failures},${cmd}"
 done
 
-if (( "${#cmd_failures}" > 0 )); then
+if (("${#cmd_failures}" > 0)); then
   printf -- '%s\n' "The following dependencies are missing: ${cmd_failures/,/}" >&2
   exit 1
 fi
 
-# Environment
-colorsfile="colors.txt"
-colorsdir="generated" && [[ ! -d $colorsdir ]] && mkdir $colorsdir
-font="Noto-Sans-Mono-Bold"
-maskimage="mask.png"
+#-----------------------------------------------------------------------------
+# Vars
+COLFILE="$1"
+FONTFILE="JetBrainsMonoNL-ExtraBold.ttf"
+MASKFILE="mask.png"
+TEMPFILE="temp.png"
+OUTDIR="out"
 
-# Generate rounded-corner rectangle mask
-convert -size 1200x720 xc:none -draw "roundrectangle 0,0,1200,720,15,15" $maskimage
-
-# should the font be white or black?
+#-----------------------------------------------------------------------------
+# Contrast for the color descriptor should be readable.
+# This function decides whether to make the font black or white.
+# More on luminance: https://stackoverflow.com/a/7253786/10059841
+# More on threshold: https://gamedev.stackexchange.com/a/38561
 white_or_black() {
-	# HEX to RGB
-	# https://stackoverflow.com/a/7253786/10059841
-	hex="$1"
-	r=$(echo "ibase=16; ${hex:0:2}/FF" | bc -l)
-	g=$(echo "ibase=16; ${hex:2:2}/FF" | bc -l)
-	b=$(echo "ibase=16; ${hex:4:2}/FF" | bc -l)
+  # HEX to RGB
+  HEX="$1"
+  RED=$(echo "ibase=16; ${HEX:0:2}/FF" | bc -l)
+  GREEN=$(echo "ibase=16; ${HEX:2:2}/FF" | bc -l)
+  BLUE=$(echo "ibase=16; ${HEX:4:2}/FF" | bc -l)
 
-	# Calculate luminance (in a rough way)
-	# https://gamedev.stackexchange.com/a/38561
-	L=$(echo "0.2126*$r + 0.7152*$g + 0.0722*$b" | bc -l)
+  # Calculate luminance (in a rough way)
+  LUMINANCE=$(echo "0.2126*${RED} + 0.7152*${GREEN} + 0.0722*${BLUE}" | bc -l)
 
-	threshold=0.5  # values from 0 to 1
-	# use_black is 1 if true, 0 otherwise
-	use_black=$(echo "$L > $threshold" | bc -l)
+  CONTRAST_THRESHOLD=0.5 # values from 0 to 1
+  # use_black is 1 if true, 0 otherwise
+  USEBLACK=$(echo "$LUMINANCE > $CONTRAST_THRESHOLD" | bc -l)
 
-	[[ $use_black -eq 1 ]] && echo "black" || echo "white"
+  [[ $USEBLACK -eq 1 ]] && echo "black" || echo "white"
 }
 
+usage() {
+  printf -- '%s\n' "Usage: $0 <COLFILE>"
+}
+
+# use a red error message and exit
+die() {
+  printf -- '\033[31m%s\033[0m\n' "$1" >&2
+  exit 1
+}
+
+#-----------------------------------------------------------------------------
+
+# Check if $1 is a file
+[[ ! -f "$COLFILE" ]] &&
+  usage &&
+  die "Colors file not provided. See colors.txt for an example."
+
+# Make the out dir
+[[ ! -d "$OUTDIR" ]] && mkdir -p "$OUTDIR" 2>/dev/null
+
+# Generate mask
+magick -size 1200x720 xc:none -draw "roundrectangle 0,0,1200,720,15,15" "$MASKFILE"
+
+COL1="+66"
+COL2="+306"
+COL3="+546"
+COL4="+786"
+COL5="+1026"
+ROW1="+680"
+ROW2="+590"
+ROW3="+550"
+ROW4="+510"
+ROW5="+470"
+# Main loop
+# Fill a small square inside each rectangle with the
+# color of the next rectangle in the row.
+# Also, colorize the hex values with the same color
+# and draw each combination in a row
 while IFS=$'\t' read -r H1 H2 H3 H4 H5; do
-	filename="$H1-$H2-$H3-$H4-$H5.png"
-	convert -size 1200x720 xc:\#ffffff \
-		-draw "fill \"#${H1}\" rectangle 0,0 240,720" \
-		-draw "fill \"#${H2}\" rectangle 240,0 480,720" \
-		-draw "fill \"#${H3}\" rectangle 480,0 720,720" \
-		-draw "fill \"#${H4}\" rectangle 720,0 960,720" \
-		-draw "fill \"#${H5}\" rectangle 960,0 1200,720" \
-		-font $font \
-		-pointsize 30 \
-		-fill "$(white_or_black "$H1")" -annotate +66+680   "$H1" \
-		-fill "$(white_or_black "$H2")" -annotate +306+680  "$H2" \
-		-fill "$(white_or_black "$H3")" -annotate +546+680  "$H3" \
-		-fill "$(white_or_black "$H4")" -annotate +786+680  "$H4" \
-		-fill "$(white_or_black "$H5")" -annotate +1026+680 "$H5" \
-		temp.png
+  FILEOUT="$H1-$H2-$H3-$H4-$H5.png"
+  magick -size 1200x720 xc:\#ffffff \
+    -draw "fill \"#${H1}\" rectangle 0,0 240,720" \
+    -draw "fill \"#${H2}\" rectangle 240,0 480,720" \
+    -draw "fill \"#${H3}\" rectangle 480,0 720,720" \
+    -draw "fill \"#${H4}\" rectangle 720,0 960,720" \
+    -draw "fill \"#${H5}\" rectangle 960,0 1200,720" \
+    \
+    -draw "fill \"#${H1}\" rectangle 320,360 400,280" \
+    -draw "fill \"#${H2}\" rectangle 560,360 640,280" \
+    -draw "fill \"#${H3}\" rectangle 800,360 880,280" \
+    -draw "fill \"#${H4}\" rectangle 1040,360 1120,280" \
+    -draw "fill \"#${H5}\" rectangle 80,360 160,280" \
+    \
+    -draw "fill \"#${H1}\" rectangle 560,280 640,200" \
+    -draw "fill \"#${H2}\" rectangle 800,280 880,200" \
+    -draw "fill \"#${H3}\" rectangle 1040,280 1120,200" \
+    -draw "fill \"#${H4}\" rectangle 80,280 160,200" \
+    -draw "fill \"#${H5}\" rectangle 320,280 400,200" \
+    \
+    -draw "fill \"#${H1}\" rectangle 800,200 880,120" \
+    -draw "fill \"#${H2}\" rectangle 1040,200 1120,120" \
+    -draw "fill \"#${H3}\" rectangle 80,200 160,120" \
+    -draw "fill \"#${H4}\" rectangle 320,200 400,120" \
+    -draw "fill \"#${H5}\" rectangle 560,200 640,120" \
+    \
+    -draw "fill \"#${H1}\" rectangle 1040,120 1120,40" \
+    -draw "fill \"#${H2}\" rectangle 80,120 160,40" \
+    -draw "fill \"#${H3}\" rectangle 320,120 400,40" \
+    -draw "fill \"#${H4}\" rectangle 560,120 640,40" \
+    -draw "fill \"#${H5}\" rectangle 800,120 880,40" \
+    \
+    -font "$FONTFILE" \
+    -pointsize 30 \
+    -fill "$(white_or_black "$H1")" -annotate "${COL1}${ROW1}" "$H1" \
+    -fill "$(white_or_black "$H2")" -annotate "${COL2}${ROW1}" "$H2" \
+    -fill "$(white_or_black "$H3")" -annotate "${COL3}${ROW1}" "$H3" \
+    -fill "$(white_or_black "$H4")" -annotate "${COL4}${ROW1}" "$H4" \
+    -fill "$(white_or_black "$H5")" -annotate "${COL5}${ROW1}" "$H5" \
+    \
+    -fill "\#$H1" -annotate "${COL2}${ROW2}" "$H1" \
+    -fill "\#$H2" -annotate "${COL3}${ROW2}" "$H2" \
+    -fill "\#$H3" -annotate "${COL4}${ROW2}" "$H3" \
+    -fill "\#$H4" -annotate "${COL5}${ROW2}" "$H4" \
+    -fill "\#$H5" -annotate "${COL1}${ROW2}" "$H5" \
+    \
+    -fill "\#$H1" -annotate "${COL2}${ROW2}" "$H1" \
+    -fill "\#$H2" -annotate "${COL3}${ROW2}" "$H2" \
+    -fill "\#$H3" -annotate "${COL4}${ROW2}" "$H3" \
+    -fill "\#$H4" -annotate "${COL5}${ROW2}" "$H4" \
+    -fill "\#$H5" -annotate "${COL1}${ROW2}" "$H5" \
+    \
+    -fill "\#$H1" -annotate "${COL3}${ROW3}" "$H1" \
+    -fill "\#$H2" -annotate "${COL4}${ROW3}" "$H2" \
+    -fill "\#$H3" -annotate "${COL5}${ROW3}" "$H3" \
+    -fill "\#$H4" -annotate "${COL1}${ROW3}" "$H4" \
+    -fill "\#$H5" -annotate "${COL2}${ROW3}" "$H5" \
+    \
+    -fill "\#$H1" -annotate "${COL4}${ROW4}" "$H1" \
+    -fill "\#$H2" -annotate "${COL5}${ROW4}" "$H2" \
+    -fill "\#$H3" -annotate "${COL1}${ROW4}" "$H3" \
+    -fill "\#$H4" -annotate "${COL2}${ROW4}" "$H4" \
+    -fill "\#$H5" -annotate "${COL3}${ROW4}" "$H5" \
+    \
+    -fill "\#$H1" -annotate "${COL5}${ROW5}" "$H1" \
+    -fill "\#$H2" -annotate "${COL1}${ROW5}" "$H2" \
+    -fill "\#$H3" -annotate "${COL2}${ROW5}" "$H3" \
+    -fill "\#$H4" -annotate "${COL3}${ROW5}" "$H4" \
+    -fill "\#$H5" -annotate "${COL4}${ROW5}" "$H5" \
+    "$TEMPFILE"
 
-	# round the corners and write the file
-	if [[ -e "$colorsdir/$filename" ]]; then
-		printf "%s\n" "File $colorsdir/$filename exists - skipping"
-	else
-		convert temp.png -matte $maskimage -compose DstIn -composite "$colorsdir/$filename" &&\
-		printf "Success: %s/%s written!\n" "$colorsdir" "$filename"
-	fi
+  # round the corners and write the file
+  if [[ -e "$OUTDIR/$FILEOUT" ]]; then
+    printf "%s\n" "File $OUTDIR/$FILEOUT exists - skipping"
+  else
+    magick "$TEMPFILE" -alpha Set "$MASKFILE" -compose DstIn -composite "$OUTDIR/$FILEOUT" &&
+      printf "Success: %s/%s written\n" "$OUTDIR" "$FILEOUT"
+  fi
+done <<<"$(cat "$COLFILE")"
 
-done <<< "$(cat $colorsfile)"
-
-rm temp.png
+rm "$TEMPFILE"
